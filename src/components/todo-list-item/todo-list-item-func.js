1ae754a90674cf3b8c7720c9fbdac005e89703ae
Serhii Honchar <sehonc@softserveinc.com>
2021-12-30 14:43:52 +0200

    add state and onLabelClick to the app

--- src/components/todo-list-item/todo-list-item-func.js
+++ src/components/todo-list-item/todo-list-item-func.js
@@ -4,20 +4,37 @@ import {Component} from "react";
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+
+    state = {
+        done: false
+    };
+
+    onLabelClick = () => {
+        // console.log(`Done: ${this.props.label}`);
+        this.setState({
+            done: true
+        });
+    };
+
     render() {
         const {label, important = false} = this.props;
+        const {done} = this.state;
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';//must be a space
+        }
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
-
         return (
-            <span className="todo-list-item">
-      <span
-          className="todo-list-item-label"
-          style={style}>
-          {label}
-      </span>
+            <span className={classNames}>
+                <span
+                    className="todo-list-item-label"
+                    style={style}
+                    onClick={this.onLabelClick}>
+                   {label}
+                </span>
 
       <button type="button"
               className="btn btn-outline-success btn-sm float-right">
