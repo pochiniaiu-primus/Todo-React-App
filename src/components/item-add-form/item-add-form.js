c1c1df21785774719f4ea155e88b0e2f5b227bdb
Serhii Honchar <sehonc@softserveinc.com>
2022-01-05 16:58:12 +0200

    add refactored  form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -15,6 +15,9 @@ export default class ItemAddForm extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         this.props.onItemAdded(this.state.label)
+        this.setState({
+            label: ''
+        })
     }
 
     render() {
@@ -24,7 +27,9 @@ export default class ItemAddForm extends Component {
                 <input type="text"
                        className="form-control"
                        onChange={this.onLabelChange}
-                       placeholder="What needs to be done"/>
+                       placeholder="What needs to be done"
+                       value={this.state.label}
+                />
                 <button className="btn btn-outline-secondary">
                     Add Item
                 </button>
